3a598137b8dbc0c77d13641b618d3de0e903804f
Asghar <asgharkrk@gmail.com>
2023-03-18 15:05:53 -0400

    Last Example Class1

--- src/class1/SecondProgram.java
+++ src/class1/SecondProgram.java
@@ -1,7 +1,9 @@
 package class1;
 
 public class SecondProgram {
-
+    /* Main is like the main door java starts reading the
+     instructions from here
+    */
     public static void main(String[] args) {
         System.out.println("Yay! i learnt how to command a computer");
     }
