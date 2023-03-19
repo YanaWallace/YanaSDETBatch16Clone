84a909da392a6e6fd2a56930e42ae27f13196088
Asghar <asgharkrk@gmail.com>
2023-03-19 13:34:48 -0400

    Class 2Facebook Example

---
+++ src/class2/FaceBookExample.java
@@ -0,0 +1,16 @@
+package class2;
+
+public class FaceBookExample {
+    public static void main(String[] args) {
+
+        String firstName="Farwa";
+        String surName="Ali";
+        // using int or long for mobile numbers
+        // can lead to issues
+        //int mobileNumber=00777_985_5070;
+        //System.out.println(mobileNumber);
+         String mobileNumber="+1777-985-5070";
+         String password="Pass@123";
+
+    }
+}
