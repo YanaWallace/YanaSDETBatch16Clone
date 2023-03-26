5d6cf9bc41e055be17be8a4bd737444fe4219af1
Asghar <asgharkrk@gmail.com>
2023-03-26 11:38:15 -0400

    If statements

--- src/class4/HW2.java
+++ src/class4/HW2.java
@@ -9,7 +9,7 @@ temperature ”
 public class HW2 {
     public static void main(String[] args) {
         int temperature=76;
-        if(temperature<=32){
+        if(temperature<32){
             System.out.println("Water will freeze with temperature");
         }else{
             System.out.println("Water will NOT freeze with temperature");
