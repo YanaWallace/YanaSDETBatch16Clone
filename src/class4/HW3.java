623c9f83e3e6ba81d1e7b12ba5e2cc8dd5a809e1
Asghar <asgharkrk@gmail.com>
2023-03-26 11:02:23 -0400

    If statements

---
+++ src/class4/HW3.java
@@ -0,0 +1,13 @@
+package class4;
+/*
+Create Java program to store 2 values for expected and actual hours.
+Your program should check
+if expected hours are more than  actual the program should print
+“You will love the course and you will succeed”,
+otherwise “Course will be very hard for you!
+ */
+public class HW3 {
+    public static void main(String[] args) {
+
+    }
+}
