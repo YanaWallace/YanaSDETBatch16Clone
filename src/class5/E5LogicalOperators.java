54e05cff1a5c338160ada1f4de6c04f91ff78842
Asghar <asgharkrk@gmail.com>
2023-03-28 21:25:33 -0400

    Class5

---
+++ src/class5/E5LogicalOperators.java
@@ -0,0 +1,13 @@
+package class5;
+
+public class E5LogicalOperators {
+    public static void main(String[] args) {
+
+        /*
+        NOT ! it flips the boolean values for example
+         */
+
+
+
+    }
+}
