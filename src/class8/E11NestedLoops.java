3906cf1f5d3e7ba988d342ce8e12f1ed87acf73f
Asghar <asgharkrk@gmail.com>
2023-04-02 14:02:18 -0400

    Class 8

---
+++ src/class8/E11NestedLoops.java
@@ -0,0 +1,19 @@
+package class8;
+
+public class E11NestedLoops {
+   // use one while and one for loop 1:59
+    /*
+    2 4 6 8 10
+    2 4 6 8 10
+    2 4 6 8 10
+    2 4 6 8 10
+     */
+   public static void main(String[] args) {
+       for (int i = 2; i < ; i++) {
+           
+       }
+   }
+    
+
+
+}
