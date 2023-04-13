0e764545b4005ff28fd767137f30827518c35c7a
Asghar <asgharkrk@gmail.com>
2023-04-12 20:11:22 -0400

    Class 9

---
+++ src/class10/Task7.java
@@ -0,0 +1,10 @@
+package class10;
+/*
+From an array of integer elements find the largest number
+ */
+public class Task7 {
+    public static void main(String[] args) {
+        // break till 8:12
+        int [] numbers={10,20,50,40,30};
+    }
+}
