fa890e93db00f67e02294ba0ba9b77253a1a5036
Asghar <asgharkrk@gmail.com>
2023-04-15 11:33:21 -0400

    Class 10

--- src/class11/E2D2Array.java
+++ src/class11/E2D2Array.java
@@ -8,6 +8,11 @@ public class E2D2Array {
                 {"Farwa", "Halima", "Alina", "Semir"}
         };
 
+        for (int i = 0; i < names.length; i++) {
+            for (int j = 0; j < names[i].length; j++) {
+                System.out.println(names[i][j]);
+            }
+        }
 
 
 
