b29204f888d1c87a6f5884028f75c9dd822af9cc
Asghar <asgharkrk@gmail.com>
2023-04-15 15:07:48 -0400

    Class 10

--- src/class11/E3D2Array.java
+++ src/class11/E3D2Array.java
@@ -8,8 +8,9 @@ public class E3D2Array {
                 {"Farwa", "Halima", "Alina", "Semir"}
         };
 
-        //Break till 1:53
+        //Break till 11:53
        for(String [] name1DArr:names2DArray){
+
            for (String name:name1DArr){
                System.out.println(name);
            }
