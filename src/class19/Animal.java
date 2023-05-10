0a9fd56e90f8bbdae48d9adb3ebd5fa9d1941c0d
Asghar <asgharkrk@gmail.com>
2023-05-09 20:28:21 -0400

    Class 19

--- src/class19/Animal.java
+++ src/class19/Animal.java
@@ -1,4 +1,5 @@
 package class19;
+
 // Create 3 classes like Dog Cat Horse define 5 properties(use default
 // access level).
 // You can use inheritance to avoid code duplication.
@@ -6,11 +7,12 @@ package class19;
 // create constructors as well in each child class to initialize those
 // properties create one object of each class and call the printInfo method
 public class Animal {
-    String name;
-    String color;
-    String breed;
-    int age;
-    double weight;
+    private String name;
+    private String color;
+    private String breed;
+    private int age;
+    private double weight;
+
     public Animal(String name, String color, String breed, int age, double weight) {
 
         this.name = name;
@@ -19,28 +21,47 @@ public class Animal {
         this.age = age;
         this.weight = weight;
     }
-    void printInfo(){
-        System.out.println(name+" "+color+" "+breed+" "+age+" "+weight);
+
+    void printInfo() {
+        System.out.println(name + " " + color + " " + breed + " " + age + " " + weight);
     }
 
 
 }
-class Dog extends Animal{
 
-   public Dog(String name, String color, String breed, int age, double weight){
-       super(name, color, breed, age, weight);
-   }
+class Dog extends Animal {
+    double price;
+
+    public Dog(String name, String color, String breed, int age, double weight, double price) {
+        super(name, color, breed, age, weight);
+        this.price = price;
+    }
+    void printInfo(){
+        super.printInfo();
+        System.out.println(price);
+    }
 }
-class Cat extends Animal{
+
+class Cat extends Animal {
 
     public Cat(String name, String color, String breed, int age, double weight) {
         super(name, color, breed, age, weight);
     }
 }
 
-class Horse extends Animal{
+class Horse extends Animal {
 
     public Horse(String name, String color, String breed, int age, double weight) {
         super(name, color, breed, age, weight);
+    }
+}
+
+class AnimalTester{
+
+
+
+    public static void main(String[] args) {
+        Dog dog=new Dog("Tom","Black","persian",10,20,200);
+        dog.printInfo();
     }
 }
