5e7e3906b7cd89ef7d41b86dcba2dabb9914557b
Asghar <asgharkrk@gmail.com>
2023-05-14 13:38:21 -0400

    Class 21

---
+++ src/class22/ArrayUtil.java
@@ -0,0 +1,15 @@
+package class22;
+
+public interface ArrayUtil {
+    //search the array for the number if found return true
+    public boolean searchArr(int [] array,int number);
+}
+
+class Main implements ArrayUtil{
+
+    @Override
+    public boolean searchArr(int[] array, int number) {
+
+        return false;
+    }
+}
