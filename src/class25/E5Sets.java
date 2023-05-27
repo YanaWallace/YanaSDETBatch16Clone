8d4a3c29682ec58d8a207ae3dd6b11f15d963933
Asghar <asgharkrk@gmail.com>
2023-05-27 13:57:19 -0400

    Class 25 to Maps

--- src/class25/E5Sets.java
+++ src/class25/E5Sets.java
@@ -6,12 +6,12 @@ public class E5Sets {
     public static void main(String[] args) {
 
         TreeSet<String> fruit=new TreeSet<>();
-        fruit.add("Mango");
-        fruit.add("Apple");
-        fruit.add("Orange");
-        fruit.add("Kiwi");
-        fruit.add("Banana");
-        fruit.add("Banana");
+        fruit.add("2");
+        fruit.add("3");
+        fruit.add("6");
+        fruit.add("9");
+        fruit.add("8");
+        fruit.add("8");
         System.out.println(fruit);
 
     }
---
+++ src/class25/E7Maps.java
@@ -0,0 +1,7 @@
+package class25;
+
+public class E7Maps {
+    public static void main(String[] args) {
+
+    }
+}
