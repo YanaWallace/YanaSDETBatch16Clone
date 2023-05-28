5fa7ad3bfe54e7a45f895c279dc5581685bb6322
Asghar <asgharkrk@gmail.com>
2023-05-28 11:24:56 -0400

    Class 25 to Maps

---
+++ src/main/java/class26/E3Maps.java
@@ -0,0 +1,26 @@
+package class26;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class E3Maps {
+
+    public static void main(String[] args) {
+        Map<String, Double> drinks = new HashMap<>();
+
+        drinks.put("Coke", 2.0);
+        drinks.put("Milk", 5.0);
+        drinks.put("Mango juice", 2.5);
+        drinks.put("Coffee", 3.0);
+        drinks.put("Tea", 3.5);
+       // remove only those which contains letter i and their price is less than 3
+       // drinks.entrySet();
+
+
+
+
+
+    }
+}
