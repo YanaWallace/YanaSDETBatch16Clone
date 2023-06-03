d42f9fbbd28ff52f8434b189f6696cb224ccd50f
Asghar <instructor@syntaxtechs.com>
2023-06-03 14:56:07 -0400

    class 27 Excel File reading

--- src/main/java/class27/E5ExcelReader.java
+++ src/main/java/class27/E5ExcelReader.java
@@ -7,30 +7,33 @@ import utils.Constants;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.*;
 
-public class E4ExcelReader {
+public class E5ExcelReader {
     public static void main(String[] args) throws IOException {
 
         FileInputStream fileInputStream=new FileInputStream(Constants.EXCEL_FILE_PATH);
         // that special call which knows how to read the data from excel files
         XSSFWorkbook xssfWorkbook=new XSSFWorkbook(fileInputStream);
         Sheet sheet=xssfWorkbook.getSheet("Sheet1");
-        for (int rows = 0; rows <sheet.getPhysicalNumberOfRows() ; rows++) {
+        List<Map<String,String>> excelData=new ArrayList<>();
+        Row headerRow= sheet.getRow(0);
+        for (int rows = 1; rows <sheet.getPhysicalNumberOfRows() ; rows++) {
             Row row=sheet.getRow(rows);
 
+            Map<String,String> rowMap=new LinkedHashMap<>();
             for (int col = 0; col < row.getPhysicalNumberOfCells() ; col++) {
-                System.out.print( row.getCell(col)+" ");
+                String key=headerRow.getCell(col).toString();
+                String value=row.getCell(col).toString();
+               rowMap.put(key,value);
             }
-            System.out.println();
+            excelData.add(rowMap);
+
+
         }
 
-        // Break till 1:54
+        System.out.println(excelData);
 
-       /* System.out.print( row.getCell(0)+" ");
-        System.out.print( row.getCell(1)+" ");
-        System.out.print( row.getCell(2)+" ");
-        System.out.print( row.getCell(3)+" ");
-        System.out.print( row.getCell(4));*/
 
 
 
